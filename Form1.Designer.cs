7dc54ee8e5f4d0783816aa6cba9284be4fc3161d
Administrator <Administrator@02-LAB01>
2025-02-24 16:59:03 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Bai15_SoThocTrenBanCo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.txto = new System.Windows.Forms.TextBox();
+            this.lsto = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "nhập số ô";
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(80, 115);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(131, 51);
+            this.btnTinh.TabIndex = 2;
+            this.btnTinh.Text = "Tính";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // txto
+            // 
+            this.txto.Location = new System.Drawing.Point(116, 65);
+            this.txto.Name = "txto";
+            this.txto.Size = new System.Drawing.Size(95, 20);
+            this.txto.TabIndex = 3;
+            // 
+            // lsto
+            // 
+            this.lsto.FormattingEnabled = true;
+            this.lsto.Location = new System.Drawing.Point(242, 24);
+            this.lsto.Name = "lsto";
+            this.lsto.Size = new System.Drawing.Size(175, 199);
+            this.lsto.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lsto);
+            this.Controls.Add(this.txto);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.TextBox txto;
+        private System.Windows.Forms.ListBox lsto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai15_SoThocTrenBanCo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            int soO;
+
+
+            if (!int.TryParse(txto.Text, out soO))
+            {
+                MessageBox.Show("Vui lòng nhập một số hợp lệ.", "Lỗi nhập liệu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            if (soO > 64)
+            {
+                MessageBox.Show("Số ô cờ phải nhỏ hơn hoặc bằng 64", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            double tongsothoc = 0;
+
+
+            lsto.Items.Clear();
+
+
+            for (int i = 0; i < soO; i++)
+            {
+                double soThoc = Math.Pow(2, i);
+                tongsothoc += soThoc;
+                lsto.Items.Add("Ô thứ " + (i + 1) + " có " + soThoc + " thóc");
+            }
+
+            // Hiển thị tổng số thóc
+            lsto.Items.Add("Tổng số thóc: " + tongsothoc);
+        }
+    }
+}
+
+
+
